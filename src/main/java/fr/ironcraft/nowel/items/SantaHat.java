94ce15136ae66142e76b63d622432e8fd20c465d
Adrien Jussak <jussak.adrien@gmail.com>
2015-11-10 21:55:18 +0100

    Santa Hat Texture

--- src/main/java/fr/ironcraft/nowel/items/SantaHat.java
+++ src/main/java/fr/ironcraft/nowel/items/SantaHat.java
@@ -1,13 +1,23 @@
 package fr.ironcraft.nowel.items;
 
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 /**
  * Created by adrien on 10/11/15.
  */
 public class SantaHat extends Item {
+
+    public SantaHat()
+    {
+        setUnlocalizedName("santa_hat");
+        setCreativeTab(CreativeTabs.tabCombat);
+        setMaxStackSize(1);
+    }
 
     @Override
     public boolean isValidArmor(ItemStack stack, int armorType, Entity entity)
@@ -15,6 +25,20 @@ public class SantaHat extends Item {
         if(armorType == 0)
             return true;
         return false;
+    }
+
+    @Override
+    public ItemStack onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn)
+    {
+        ItemStack itemstack1 = playerIn.getCurrentArmor(3);
+
+        if (itemstack1 == null)
+        {
+            playerIn.setCurrentItemOrArmor(4, itemStackIn.copy()); //Forge: Vanilla bug fix associated with fixed setCurrentItemOrArmor indexs for players.
+            itemStackIn.stackSize = 0;
+        }
+
+        return itemStackIn;
     }
 
 }
